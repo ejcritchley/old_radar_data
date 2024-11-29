7d5d78c519a5dd48f2d4cde275dcee3202bc4515
ejcritchley <emma.critchley@nina.no>
2024-11-29 16:00:18 +0100

    Added more filtering steps

--- Radar_data_connect.R
+++ Radar_data_connect.R
@@ -13,6 +13,7 @@ library(ggplot2)
 #library(NinaR)
 library(leaflet)
 
+
 #options(viewer = NULL) 
 #mapviewOptions(viewer.suppress=TRUE)
 
@@ -405,4 +406,193 @@ map =
 map
 
 leaflet(map)
+
+# Create a radius around the radar location 
+
+radar_location = 
+  st_read(con, 
+          query = 
+            "SELECT * FROM m202210.radar 
+       WHERE location_name = 'Fedje'
+       AND timestamp = '2022-10-03 13:43:20.306';",
+          geometry_column = 'position'
+  )
+
+mapview(st_zm(radar_location))
+
+radar_location <- st_zm(radar_location)
+
+
+# Create radar inclusion zone
+
+## Projections
+wgs84_epsg <- 4326 
+loc_epsg <- 25833
+
+## Radar coordinates [degrees]
+radar_lon <- 4.185345
+radar_lat <- 52.427827
+radar <- st_sfc(st_point(c(radar_lon, radar_lat)),crs=wgs84_epsg)
+
+#OR 
+
+radar <- radar_location
+
+radar_utm <-st_transform(radar_location, crs = loc_epsg)
+
+## Set the minimum and maximum distance from the radar for reliable bird detection
+mind <- 100 ## [metres]
+maxd <- 5000 ## [metres]
+
+min_buffer <- st_buffer(radar_utm, dist = 10)
+
+max_buffer <- st_buffer(radar_utm, dist = 5000)
+
+mapview(radar_utm) + min_buffer + max_buffer
+
+# polygons to exclude for noise
+
+noise_poly <- st_read("P:/312202_visavis/WP2/Previous_radar_migration/GIS/radar_inclusion_zone.shp")
+
+noise_poly_utm <- st_transform(noise_poly, crs = loc_epsg)
+
+mapview(noise_poly_utm)
+
+aoi <- st_difference(max_buffer, min_buffer)
+
+mapview(aoi)
+
+noise_poly_utm <- st_union(noise_poly_utm)
+
+aoi2 <- st_difference(aoi, noise_poly_utm)
+
+mapview(aoi2)
+
+## Two corners of the radar window are unavailable in our data
+## The corner of the radar window the turbine blocks the radar
+## The corner of overlap between the horizontal and vertical radar (data contains horizontal-only tracks)
+blocked_corners <- rbind(c(287,30),
+                         c(115,135))
+
+
+## The radar is situated within a wind farm
+## Wind farm name (for coordinate extraction from main file)
+location <- 'Fedje'
+
+# Load some sample Fedge tracks
+
+test_tracks = 
+  st_read(con, 
+          query = 
+            "WITH modified_tracks AS (
+            SELECT *,
+            ST_Length(trajectory) as track_length, 
+      	    ST_Distance(ST_StartPoint(trajectory),
+	          ST_EndPoint(trajectory)) as track_distance
+            FROM m202210.track 
+       WHERE timestamp_start > '2022-10-10 09:00:00'
+       AND timestamp_end < '2022-10-10 14:00:00'
+       AND avg_rcs > -40
+       AND avg_rcs < 0
+       AND score > 0.85
+       AND airspeed > 5
+       AND airspeed < 30
+       AND classification_id BETWEEN '6' AND '10'),
+	tortuosity_table as (
+select
+	*,
+	track_distance / track_length as tortuosity
+from
+	modified_tracks
+)
+select
+	*
+from
+	tortuosity_table
+where
+	tortuosity > 0.65;",
+          geometry_column = 'trajectory')
+
+
+mapview(test_tracks)
+
+test_tracks_utm <- st_transform(test_tracks, crs = loc_epsg)
+# 
+# test_tracks_utm$start_point <- lwgeom::st_startpoint(test_tracks_utm)
+# 
+# test_tracks_utm$start_point <- st_transform(test_tracks_utm$start_point, crs = loc_epsg)
+# 
+# mapview(test_tracks_utm$start_point)
+# 
+# mapview(test_tracks_utm)
+
+# filter tracks over 200 metres 
+
+test_tracks_utm <- test_tracks_utm %>%
+  mutate(start_point = lwgeom::st_startpoint(test_tracks_utm), 
+         end_point = lwgeom::st_endpoint(test_tracks_utm),
+         track_length_m = as.numeric(st_length(trajectory)),
+         track_time = as.numeric(hms::as_hms(difftime(
+           time1 = timestamp_end,
+           time2 = timestamp_start,
+           units = "secs")))
+  )
+
+test_tracks_utm <- test_tracks_utm %>%
+  filter(track_length_m > 200) %>%
+  filter(track_time > 20)
+
+mapview(test_tracks_utm)
+
+
+
+# check if line starts outside the area of inclusion
+
+mapview(aoi2)
+
+# create seperate object with just start points
+
+track_start <- st_as_sf(test_tracks_utm$start_point)
+
+track_start <- cbind(track_start,test_tracks_utm$id)
+
+mapview(track_start) + aoi2
+
+# check if start falls within polygon
+
+point_aoi <- as.data.frame(t(st_contains(aoi2,track_start, sparse = FALSE)))
+
+track_start <- cbind(track_start,point_aoi) # create table
+
+track_exclude <- track_start %>%  # filter points outside polygon
+  filter(V1 == 'FALSE')
+
+mapview(track_exclude) + aoi2
+
+# create table of points outside the polygon
+
+track_exclude_table <- track_exclude %>%
+  select(test_tracks_utm.id, V1)
+  rename(id = test_tracks_utm.id, in_polygon = v1) 
+
+track_exclude_table <- cbind(track_exclude$test_tracks_utm.id, track_exclude$V1)
+
+# add exclusion field to track table
+
+test_tracks_utm2 <- cbind(test_tracks_utm,track_start$V1)
+
+exclude_tracks <- test_tracks_utm2 %>%
+  filter(track_start.V1 == 'FALSE')
+
+mapview(exclude_tracks) + aoi2
+
+# create table of tracks to include
+
+include_tracks <- test_tracks_utm2 %>%
+  filter(track_start.V1 == 'TRUE')
+
+mapview(include_tracks, color = 'green', col.regions = ) + exclude_tracks  + aoi2
+
+mapview(include_tracks) + aoi2
+
 
