dd861775ca789e59e70227e15b81d3bda7a4a5e6
ejcritchley <emma.critchley@nina.no>
2024-12-02 13:42:22 +0100

    Updated R code and metadata file

---
+++ track_aoi_exclude.R
@@ -0,0 +1,35 @@
+
+# Code to exclude radar tracks that start outside of the area of interest
+
+# read in polygon showing area of inclusion
+
+aoi <- st_read("P:/312202_visavis/WP2/Previous_radar_migration/GIS/radar_inclusion_poly.shp")
+
+# create separate spatial object with just start points
+
+test_tracks <- test_tracks %>%
+  mutate(start_point = lwgeom::st_startpoint(test_tracks))
+
+track_start <- st_as_sf(test_tracks$start_point) # make spatial object
+
+track_start <- cbind(track_start,test_tracks$id) # add id fields
+
+# check if start point falls within aoi polygon
+
+point_aoi <- as.data.frame(t(st_contains(aoi,track_start, sparse = FALSE)))
+
+track_start <- cbind(track_start,point_aoi) # create table with start points
+
+# add exclusion field to track table
+
+test_tracks <- cbind(test_tracks,track_start$V1)
+
+test_tracks <- test_tracks %>%
+  rename(include = track_start.V1)
+
+# create table of tracks to include
+
+include_tracks <- test_tracks %>%
+  filter(include == 'TRUE')
+
+mapview(include_tracks) + aoi
